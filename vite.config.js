6ad7270cdfbc3239be432a1227c3018e1842e1fc
Chavisky D <auukunn.bkk@gmail.com>
2026-01-29 21:45:56 +0700

    vite config scglamreception

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/scGlamLiff-reception/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/scGlamLiff-reception/' : '/',
   plugins: [react()],
   server: {
     proxy: {
       '/api': 'http://localhost:5050',
     },
   },
-})
+}))
